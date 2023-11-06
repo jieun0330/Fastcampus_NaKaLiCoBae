5ffbde3043061e36f82806f398d20ba4f5569b8b
jieun <jieun_0330@naver.com>
2023-11-06 11:24:18 +0900

    first commit

---
+++ Fastcampus_NaKaLiCoBae/Fastcampus_NaKaLiCoBaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fastcampus_NaKaLiCoBaeApp.swift
+//  Fastcampus_NaKaLiCoBae
+//
+//  Created by 박지은 on 2023/11/06.
+//
+
+import SwiftUI
+
+@main
+struct Fastcampus_NaKaLiCoBaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
