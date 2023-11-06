024f1d9f516abf90c1a464b92906f8595a99d05f
jieun <jieun_0330@naver.com>
2023-11-06 11:36:56 +0900

    [Ch 2. 문법의 첫 걸음] 01. 변수

--- Fastcampus_NaKaLiCoBae/ContentView.swift
+++ Fastcampus_NaKaLiCoBae/ContentView.swift
@@ -8,19 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var name: String = "존"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(name)님 안녕하세요!")
+            Text("\(name)님의 포인트는 343점 입니다.")
+            Text("\(name)님 안녕히가세요")
+            Text(name)
+//            Text("2"+"2"-"2")
+//            Int(2+2)
         }
         .padding()
     }
 }
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
