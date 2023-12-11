a4cc7e9be59224e706c88a997e08182e19a5c6d1
jieun <jieun_0330@naver.com>
2023-12-11 20:01:18 +0900

    [Ch 4. 문법 활용해서 코딩하기] 01. 예외처리

---
+++ Fastcampus_NaKaLiCoBae/Ch 4. 문법 활용해서 코딩하기/Exception.swift
@@ -0,0 +1,54 @@
+//
+//  Exception.swift
+//  Fastcampus_NaKaLiCoBae
+//
+//  Created by 박지은 on 12/11/23.
+//
+
+import SwiftUI
+
+struct Exception: View {
+    
+    @State var inputNumber = ""
+    @State var resultNumber: Float = 0
+    
+    var body: some View {
+        VStack {
+            TextField("나눌 숫자를 입력해주세요", text: $inputNumber)
+            Text("나눈 결과는 다음과 같아요 \(resultNumber)")
+            Button(action: {
+                
+                do {
+                    try resultNumber = devideTen(with: (Float(inputNumber) ?? 1.0))
+                } catch DivideError.dividedByZero {
+                    print("0으로 나누었대요")
+                    resultNumber = 0
+                    inputNumber = "잘못된 입력입니다"
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }, label: {
+                Text("나누기")
+            })
+        }
+    }
+    
+    func devideTen(with inputNumber: Float) throws -> Float {
+        var result: Float = 0
+        
+        if inputNumber == 0 {
+            throw DivideError.dividedByZero
+        } else {
+            result = 10 / inputNumber
+        }
+        return result
+    }
+}
+
+enum DivideError: Error {
+    case dividedByZero
+}
+
+#Preview {
+    Exception()
+}
