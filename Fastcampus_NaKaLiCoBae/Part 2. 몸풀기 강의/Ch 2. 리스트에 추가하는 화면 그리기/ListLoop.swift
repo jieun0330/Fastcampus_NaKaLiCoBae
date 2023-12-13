f25cc33bc54c2886a7975d1971ca0bb2bbd29465
jieun <jieun_0330@naver.com>
2023-12-13 19:29:13 +0900

    [Ch 1. 리스트에 추가하는 화면 그리기] 03. 리스트의 추가와 삭제

--- Fastcampus_NaKaLiCoBae/Part 2. 몸풀기 강의/Ch 2. 리스트에 추가하는 화면 그리기/ListLoop.swift
+++ Fastcampus_NaKaLiCoBae/Part 2. 몸풀기 강의/Ch 2. 리스트에 추가하는 화면 그리기/ListLoop.swift
@@ -17,7 +17,7 @@ struct Fruit: Hashable {
 
 struct ListLoop: View {
     
-    var favoriteFruits = [
+    @State var favoriteFruits = [
         Fruit(name: "Apple", matchFruitName: "Banana", price: 1000),
         Fruit(name: "Banana", matchFruitName: "Banana", price: 3000),
         Fruit(name: "Cherry", matchFruitName: "Double Kiwi", price: 4000),
@@ -31,18 +31,44 @@ struct ListLoop: View {
 //    var price = ["1000", "3000", "4000", "2400", "8000"]
 //    var count = 0
     
+    @State var fruitName: String = ""
+    
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(favoriteFruits, id: \.self) { fruit in
-                    VStack(alignment: .leading) {
-                        Text("name: \(fruit.name)")
-                        Text("matchFruit: \(fruit.matchFruitName)")
-                        Text("price:\(fruit.price)")
+            
+            VStack {
+                HStack {
+                    TextField("insert fruit name", text: $fruitName)
+                    Button(action: {
+                        favoriteFruits.append(Fruit(name: fruitName,
+                                                    matchFruitName: "Apple",
+                                                    price: 1000))
+                    }, label: {
+                        Text("insert")
+                            .padding()
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    })
+                }
+                .padding()
+                
+                List {
+                    ForEach(favoriteFruits, id: \.self) { fruit in
+                        VStack(alignment: .leading) {
+                            Text("name: \(fruit.name)")
+                            Text("matchFruit: \(fruit.matchFruitName)")
+                            Text("price:\(fruit.price)")
+                        }
+                    }
+                    .onDelete { indexSet in
+//                        favoriteFruits.remove(atOffsets: indexSet)
+                        favoriteFruits.remove(at: 0)
                     }
                 }
+                .navigationTitle("Fruit List")
+                
             }
-            .navigationTitle("Fruit List")
         }
     }
 }
