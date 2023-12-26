26a7a41c544e3bfaaf3cf3a1a2886460a44dfb8f
jieun <jieun_0330@naver.com>
2023-12-26 10:51:46 +0900

    [Ch 3. 여러개 화면 그려서 이어 붙이기] 01. 여러 화면을 이동하기

---
+++ Fastcampus_NaKaLiCoBae/Part 2. 몸풀기 강의/Ch 3. 여러개 화면 그려서 이어 붙이기/Modal.swift
@@ -0,0 +1,18 @@
+//
+//  Modal.swift
+//  Fastcampus_NaKaLiCoBae
+//
+//  Created by 박지은 on 12/26/23.
+//
+
+import SwiftUI
+
+struct Modal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Modal()
+}
