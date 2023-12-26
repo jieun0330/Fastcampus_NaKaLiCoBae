a5633713e5b6f3db2e1853cc9968ef08ad7de4af
jieun <jieun_0330@naver.com>
2023-12-26 11:31:32 +0900

    [Ch 3. 여러개 화면 그려서 이어 붙이기] 03. 탭뷰로 그려보기

---
+++ Fastcampus_NaKaLiCoBae/Part 2. 몸풀기 강의/Ch 3. 여러개 화면 그려서 이어 붙이기/MyTab.swift
@@ -0,0 +1,18 @@
+//
+//  MyTab.swift
+//  Fastcampus_NaKaLiCoBae
+//
+//  Created by 박지은 on 12/26/23.
+//
+
+import SwiftUI
+
+struct MyTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyTab()
+}
